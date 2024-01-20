5f85e1cea20f8a0144db21f4f416807b9be5afc8
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2024-01-20 09:49:11 +0100

    scaffholding + print in console

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template></template>
+
+<script>
+import { store } from './data/store';
+import axios from 'axios';
+export default {
+  name: "App",
+  components: [
+    ////////////////////////////////
+  ],
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    getAllProjects() {
+      axios.get(store.apiUrl + 'projects')
+        .catch(error => { throw error; })
+        .then((res) => {
+          store.projects = res.data.results
+          console.log(store.projects);
+        })
+    }
+
+  },
+  mounted() {
+    this.getAllProjects();
+  }
+}
+</script>
+
+<style lang="scss"></style>
